771ac99acef1118e072e70fb8eec655f604cbee0
Priyesh ML <priyeshvayala@gmail.com>
2023-10-07 02:24:57 +0530

    twentyseventh commit

--- FrontEnd/src/Components/User/Message/Message.tsx
+++ FrontEnd/src/Components/User/Message/Message.tsx
@@ -41,8 +41,8 @@ interface ImessageProps {
 }
 
 // let ENDPOINT = process.env.SERVER_URL
-let ENDPOINT = "http://localhost:5000";
-// let ENDPOINT = process.env.SERVER_URL;
+// let ENDPOINT = "http://localhost:5000";
+let ENDPOINT = process.env.SERVER_URL;
 
 let socket: Socket, selectedChatCompare: IuserChatList;
 
@@ -90,7 +90,7 @@ const Message: React.FC<ImessageProps> = ({
   const[otherUserPeerId,SetOtherUserPeerId] = useState<string>('')
   //socket io connection
   useEffect(() => {
-      socket = io(ENDPOINT);
+      socket = io(ENDPOINT as string);
       socket.emit("setup", user.userName);
       socket.on("connected", () => setSocketConnected(true));
       socket.on(
